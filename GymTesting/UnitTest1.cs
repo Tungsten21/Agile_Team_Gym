382076bcdd41f2a119b2189f610f373021e7c4e7
Tungsten21 <rossodonegan123@gmail.com>
2020-02-11 09:32:39 +0000

    Added test class and normal class for trainer

---
+++ GymTesting/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GymClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GymTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsTrainer ATrainer = new clsTrainer();
+            //test to see that it exists
+            Assert.IsNotNull(ATrainer);
+        }
+    }
+}
