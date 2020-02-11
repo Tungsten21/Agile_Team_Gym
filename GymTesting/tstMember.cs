c53f6d72e605adc6f5ed39434c6235399c42358e
p17224299 <p17224299@GWHXX581WIN011W>
2020-02-11 12:34:56 +0000

    clsmember added

---
+++ GymTesting/tstMember.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GymClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GymTesting
+{
+    [TestClass]
+    public class tstMember
+    {
+        [TestMethod]
+        
+            public void InstanceOK()
+            {
+                //create an instance of the class we want to create
+                clsMember aMember = new clsMember();
+                //test to see if it exists
+                Assert.IsNotNull(aMember);
+            }
+        }
+    }
+
