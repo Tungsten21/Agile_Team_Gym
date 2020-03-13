ab34d15851bd27ab8cb6a424f262f9bdd2cc53fc
Tungsten21 <rossodonegan123@gmail.com>
2020-03-13 11:18:21 +0000

    added all anood's changes to my local master branch

--- GymClasses/clsSession.cs
+++ GymClasses/clsSession.cs
@@ -1,6 +1,137 @@
-﻿namespace GymTesting
+﻿using System;
+
+namespace GymTesting
 {
     public class clsSession
     {
+        private int mSessionID;
+        private int mTrainerID;
+        private int mBranchID;
+        private string mSessionType;
+        private double mCost;
+        private DateTime mDateTime;
+        private bool mEquipmentRequired;
+
+        public int SessionID
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mSessionID;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mSessionID = value;
+            }
+        }
+
+        public int TrainerID
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mTrainerID;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mTrainerID = value;
+            }
+        }
+        public int BranchID
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mBranchID;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mBranchID = value;
+            }
+        }
+        public String SessionType
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mSessionType;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mSessionType = value;
+            }
+        }
+
+        public DateTime DateTime
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mDateTime;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mDateTime = value;
+            }
+        }
+        public Double Cost
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mCost;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mCost = value;
+            }
+        }
+
+        public bool EquipmentRequired
+        {
+            get
+            {
+                return mEquipmentRequired;
+            }
+            set
+            {
+                mEquipmentRequired = value;
+            }
+        }
+        public bool Find(int SessionID)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the TrainerID to search for
+            DB.AddParameter("@SessionID", SessionID);
+            //execute the stored procedure
+            DB.Execute("sproc_SessionTable_FilterBySessionID");
+            //if one record is found (there should be either one or zero!)
+            if (DB.Count == 1)
+            {
+                //set the private data members to th test data value
+                mSessionID = Convert.ToInt32(DB.DataTable.Rows[0]["SessionID"]);
+                mTrainerID = Convert.ToInt32(DB.DataTable.Rows[0]["TrainerID"]);
+                mBranchID = Convert.ToInt32(DB.DataTable.Rows[0]["BranchID"]);
+                mSessionType = Convert.ToString(DB.DataTable.Rows[0]["SessionType"]);
+                mCost = Convert.ToDouble(DB.DataTable.Rows[0]["Cost"]);
+                mEquipmentRequired = Convert.ToBoolean(DB.DataTable.Rows[0]["EquipmentRequired"]);
+                mDateTime = Convert.ToDateTime(DB.DataTable.Rows[0]["DateTime"]);
+                // always return true
+                return true;
+            }
+            //if no record was found
+            else
+            {
+                //return false indicating a probelm
+                return false;
+            }
+        }
     }
 }
---
+++ GymFrontOffice/ASession.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ASession.aspx.cs" Inherits="ASession" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form2" runat="server">
+        <div style="height: 397px">
+            Session ID:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <asp:TextBox ID="txtSessionID" runat="server" Height="16px" Width="150px" TextMode="Number"></asp:TextBox>
+            <br />
+            <br />
+            Trainer ID:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtTrainerID" runat="server" Height="16px" Width="150px" TextMode="Number"></asp:TextBox>
+            <br />
+            <br />
+            Branch ID:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtBranchID" runat="server" Height="16px" Width="150px" TextMode="Number"></asp:TextBox>
+            <br />
+            <br />
+            Type:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+            <asp:TextBox ID="txtType" runat="server" Height="16px" Width="150px"></asp:TextBox>
+            <br />
+            <br />
+             <asp:Label ID="lblDateTime" runat="server" Text="DateTime"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtDateTime" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Cost:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtCost" runat="server" Height="16px" Width="150px" ></asp:TextBox>
+            <br />
+            <br />
+             <asp:CheckBox ID="chkEquipmentRequired" runat="server" Text="Equipment Required" TextAlign="Left" />
+              <br />
+              <br />
+            <asp:Button ID="Ok" runat="server"  OnClick="Button1_Click" Text="OK" Width="103px" />
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Cancel" runat="server" Text="Cancel" Width="103px" OnClick="Cancel_Click" />
+        &nbsp;&nbsp;&nbsp;
+           
+            <asp:Button ID="Find" runat="server" Text="Find"  OnClick="Find_Click" Width="103px" />
+        </div>
+    </form>
+</body>
+</html>
