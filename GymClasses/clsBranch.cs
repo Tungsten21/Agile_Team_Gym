f1e29277279a212be0f2c12b49be02ec17fc011d
Ahmed Osman <P2510227@my365.dmu.ac.uk>
2020-05-17 14:46:59 +0100

    just added branch stuff

---
+++ GymClasses/clsBranch.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+namespace GymClasses
+{
+    public class clsBranch
+    {
+
+        private Int32 mGymID;
+        private string mGymCity;
+        private string mGymAddress;
+        private DateTime mDateOpened;
+        private int mSessionCount;
+        private bool mAmmenities;
+
+
+        public int GymID
+        {
+            get
+            {
+                return mGymID;
+            }
+            set
+            {
+                mGymID = value;
+            }
+        }
+
+        public string GymCity
+        {
+            get
+            {
+                return mGymCity;
+            }
+            set
+            {
+                mGymCity = value;
+            }
+        }
+
+        public string GymAddress
+        {
+            get
+            {
+                return mGymAddress;
+            }
+            set
+            {
+                mGymAddress = value;
+            }
+        }
+
+        public DateTime DateOpened
+        {
+            get
+            {
+                return mDateOpened;
+            }
+            set
+            {
+                mDateOpened = value;
+            }
+        }
+
+        public int SessionCount
+        {
+            get
+            {
+                return mSessionCount;
+            }
+            set
+            {
+                mSessionCount = value;
+            }
+        }
+
+        public bool Ammenities
+        {
+            get
+            {
+                return mAmmenities;
+            }
+            set
+            {
+                mAmmenities = value;
+            }
+        }
+
+
+        public bool Find(int GymID)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+
+            //add the parameter for the GymID to search for
+            DB.AddParameter("@GymID", GymID);
+
+            DB.Execute("sproc_BranchTable_FilterByGymID");
+
+            if (DB.Count == 1)
+            {
+
+                mGymID = Convert.ToInt32(DB.DataTable.Rows[0]["GymID"]);
+                mGymCity = Convert.ToString(DB.DataTable.Rows[0]["GymCity"]);
+                mGymAddress = Convert.ToString(DB.DataTable.Rows[0]["GymAddress"]);
+                mDateOpened = Convert.ToDateTime(DB.DataTable.Rows[0]["DateOpened"]);
+                mSessionCount = Convert.ToInt32(DB.DataTable.Rows[0]["SessionCount"]);
+                mAmmenities = Convert.ToBoolean(DB.DataTable.Rows[0]["Ammenities"]);
+                //return that everything worked OK
+                return true;
+            }
+            //if no record was found
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public string Valid(string GymCity, string GymAddress, string DateOpened)
+        {
+            //create a string variable to store the error
+            String Error = "";
+            DateTime DataTemp;
+
+            //if the GymCity is blank
+
+            if (GymCity.Length == 0)
+            {
+
+                Error = Error + "Please enter a city name : ";
+            }
+            if (GymCity.Length == 1)
+            {
+                Error = Error + "Please enter a valid city name : ";
+            }
+
+
+            try
+            {
+                DataTemp = Convert.ToDateTime(DateOpened);
+
+
+
+                if (DataTemp > DateTime.Now.Date)
+                {
+                    Error = Error + "The date cannot be in the future : ";
+                }
+            }
+            catch
+            {
+                Error = Error + "Please enter a valid date following the correct format: yyyy/mm/dd : ";
+            }
+
+            if (GymAddress.Length < 5)
+            {
+                Error = Error + "Please enter a valid Gym Post Code : ";
+            }
+            if (GymAddress.Length > 15)
+            {
+                Error = Error + "Please enter an Gym Post Code with less then 15 characters long : ";
+            }
+
+            return Error;
+        }
+
+    }
+}
+
+
+
+
---
+++ GymClasses/clsBranchCollection .cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GymClasses
+{
+    public class clsBranchCollection
+    {
+
+        
+        List<clsBranch> mBranchList = new List<clsBranch>();
+        
+        clsBranch mThisBranch = new clsBranch();
+
+        public clsBranchCollection()
+        {
+           
+            clsDataConnection DB = new clsDataConnection();
+            
+            DB.Execute("sproc_BranchTable_SelectAll");
+            
+            PopulateArray(DB);
+        }
+        public List<clsBranch> BranchList
+        {
+            get
+            {
+                
+                return mBranchList;
+            }
+            set
+            {
+              
+                mBranchList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mBranchList.Count;
+            }
+            set
+            {
+
+            }
+        }
+        public clsBranch ThisBranch
+        {
+            get
+            {
+               
+                return mThisBranch;
+            }
+            set
+            {
+                mThisBranch = value;
+            }
+        }
+        public int Add()
+        {
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@GymCity", mThisBranch.GymCity);
+            DB.AddParameter("@GymAddress", mThisBranch.GymAddress);
+            DB.AddParameter("@DateOpened", mThisBranch.DateOpened);
+            DB.AddParameter("@SessionCount", mThisBranch.SessionCount);
+            DB.AddParameter("@Ammenities", mThisBranch.Ammenities);
+            
+            return DB.Execute("sproc_BranchTable_Insert");
+        }
+
+        public void Delete()
+        {
+
+            
+            clsDataConnection DB = new clsDataConnection();
+            
+            DB.AddParameter("@GymID", mThisBranch.GymID);
+           
+            DB.Execute("sproc_BranchTable_Delete");
+        }
+
+        public void Update()
+        {
+           
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@GymID", mThisBranch.GymID);
+            DB.AddParameter("@GymCity", mThisBranch.GymCity);
+            DB.AddParameter("@GymAddress", mThisBranch.GymAddress);
+            DB.AddParameter("@DateOpened", mThisBranch.DateOpened);
+            DB.AddParameter("@SessionCount", mThisBranch.SessionCount);
+            DB.AddParameter("@Ammenities", mThisBranch.Ammenities);
+            
+            DB.Execute("sproc_Branchtable_Update");
+        }
+
+        public void ReportByDateOpened(string DateOpened)
+        {
+            
+            clsDataConnection DB = new clsDataConnection();
+           
+            DB.AddParameter("@DateOpened", DateOpened);
+   
+            DB.Execute("sproc_BranchTable_FilterByDateOpened");
+          
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+
+            Int32 Index = 0;
+           
+            Int32 RecordCount;
+            
+            RecordCount = DB.Count;
+           
+            mBranchList = new List<clsBranch>();
+        
+            while (Index < RecordCount)
+            {
+                //create a blank Branch
+                clsBranch ABranch = new clsBranch();
+                
+                ABranch.GymID = Convert.ToInt32(DB.DataTable.Rows[Index]["GymhID"]);
+                ABranch.GymCity = Convert.ToString(DB.DataTable.Rows[Index]["GymCity"]);
+                ABranch.GymAddress = Convert.ToString(DB.DataTable.Rows[Index]["GymAddress"]);
+                ABranch.DateOpened = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateOpened"]);
+                ABranch.SessionCount = Convert.ToInt32(DB.DataTable.Rows[Index]["SessionCount"]);
+                ABranch.Ammenties = Convert.ToBoolean(DB.DataTable.Rows[Index]["Ammenties"]);
+
+                mBranchList.Add(ABranch);
+                
+                Index++;
+            }
+        }
+    }
+}
+
